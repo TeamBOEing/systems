878a2fcd1e7ba7df5faba7a8dbce4d994f66b76b
Corbin Murrow <corbin.murrow@gmail.com>
2015-09-25 10:49:38 -0500

    Created Light Sensor Test Project

---
+++ LS_Test/LS_Test.ino
@@ -0,0 +1,23 @@
+/*
+ * Light Sensor Test
+ * Author: Corbin Murrow
+ * Date: 25 September 2015
+ * Version: 1.0
+ * 
+ * This softwrae will test the photoresistors by reading the
+ * value from an analog input pin and printing the value to the
+ * serial monitor in the format "<Left/Right> Light: <value>".
+ * 
+ * ======= VERSION HISTORY =======
+ * Version 1.0: Created file and added header - CM - 25 September 2015
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
