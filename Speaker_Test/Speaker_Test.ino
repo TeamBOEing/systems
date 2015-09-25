70e654472ada78d4f537f5bbc1308571e00a0911
Corbin Murrow <corbin.murrow@gmail.com>
2015-09-25 13:39:40 -0500

    Created Speaker_Test Sketch

---
+++ Speaker_Test/Speaker_Test.ino
@@ -0,0 +1,24 @@
+/*
+ * Speaker Test
+ * Author: Corbin Murrow
+ * Date: 25 September 2015
+ * Version: 1.0
+ * 
+ * This software will output multiple tones via an analog output 
+ * pin from the ATmega microprocessor. This test is necessary to
+ * verify that the final requirement of the BOEbot project
+ * can be fulfilled.
+ * 
+ * ======= VERSION HISTORY =======
+ * Version 1.0: Created file and header - CM - 25 September 2015
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
