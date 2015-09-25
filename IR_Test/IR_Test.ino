2765375c70e9ba0cd327eb6a7e2b1cd52321a26e
Corbin Murrow <corbin.murrow@gmail.com>
2015-09-25 10:45:18 -0500

    Created PWM Test and IR Test Project

---
+++ IR_Test/IR_Test.ino
@@ -0,0 +1,24 @@
+/*
+ * IR Test
+ * Author: Corbin Murrow
+ * Date: 25 September 2015
+ * Version: 1.0
+ * 
+ * This software will test the left and right IR sensors. The 
+ * softwre will check each sensor every half second and when
+ * a sensor detects something it will print out to the serial
+ * monitor: "<Left/Right> IR: 1".
+ * 
+ * ======= VERSION HISTORY =======
+ * Version 1.0: Created file, added header - CM - 25 September 2015
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ PWM_Test/PWM_Test.ino
@@ -0,0 +1,24 @@
+/*
+ * PWM Test
+ * Author: Corbin Murrow
+ * Date: 25 September 2015
+ * Version: 1.0
+ * 
+ * This software tests PWM output to turn the continuous 
+ * rotation servos that the BOEbot uses as motors. Left and 
+ * right motor will turn forward and backward simultaneously 
+ * as well as left forward/right backward and vice versa.
+ * 
+ * ======= VERSION HISTORY =======
+ * Version 1.0: Created file - CM - 25 September 2015
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
