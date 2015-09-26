48a2685f878c200c8fe43e875149778563ec4a7f
Corbin Murrow <corbin.murrow@gmail.com>
2015-09-26 15:27:04 -0500

    Code to play C Major scale on the speaker

--- Speaker_Test/Speaker_Test.ino
+++ Speaker_Test/Speaker_Test.ino
@@ -1,8 +1,8 @@
 /*
  * Speaker Test
  * Author: Corbin Murrow
- * Date: 25 September 2015
- * Version: 1.0
+ * Date: 26 September 2015
+ * Version: 1.1
  * 
  * This software will output multiple tones via an analog output 
  * pin from the ATmega microprocessor. This test is necessary to
@@ -10,15 +10,40 @@
  * can be fulfilled.
  * 
  * ======= VERSION HISTORY =======
+ * Version 1.1: Played C Major Scale - CM - 26 September 2015
  * Version 1.0: Created file and header - CM - 25 September 2015
  */
 
-void setup() {
-  // put your setup code here, to run once:
+#define SPEAKERPIN 17
 
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Play a scale!\n");
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
+  Serial.println("Speaker on.");
+  tone(SPEAKERPIN, 262, 500); //C4
+  delay(500);
+  tone(SPEAKERPIN, 294, 500); //D4
+  delay(500);
+  tone(SPEAKERPIN, 330, 500); //E4
+  delay(500);
+  tone(SPEAKERPIN, 349, 500); //F4
+  delay(500);
+  tone(SPEAKERPIN, 392, 500); //G4
+  delay(500);
+  tone(SPEAKERPIN, 440, 500); //A4
+  delay(500);
+  tone(SPEAKERPIN, 494, 500); //B4
+  delay(500);
+  tone(SPEAKERPIN, 523, 500); //C4
+  delay(500);
+  Serial.println("Speaker off.");
 
+  delay(5000);
 }
