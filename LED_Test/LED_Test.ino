23285897afadee11ba66c4a27db15c9726f2efd8
Corbin Murrow <corbin.murrow@gmail.com>
2015-09-28 16:51:45 -0500

    Updated const ints to #defines
    For items that we don't want students to use we want to use #defines to
    differentiate in the student's mind between variables and defines. They
    can touch variables. They can't touch #defines.

--- LED_Test/LED_Test.ino
+++ LED_Test/LED_Test.ino
@@ -1,8 +1,8 @@
 /* 
  *  LED Test Software
- *  Author: Corbin Murrow
- *  Date: 25 September 2015
- *  Version: 1.1
+ *  Author: Corbin Murrow, Cody Crossley
+ *  Date: 28 September 2015
+ *  Version: 1.3
  *  
  *  This software does turns a red and green LED 
  *  on that are connected to physical pins 18 and 19 
@@ -10,14 +10,16 @@
  *  monitor when the LEDs turn on or off.
  *  
  *  ====== VERSION HISTORY ======
+ *  Version 1.3: Updated consts to defines for pin #s - CM - 28 September 2015
  *  Version 1.2: Included use of constants and updated pin number - CC - 28 September 2015
  *  Version 1.1: Updated file header - CM - 25 September 2015
  *  Version 1.0: Created project - CM - 20 September 2015
  */
 
-const int LED_PIN_GREEN = 7;
-const int LED_PIN_RED = 8;
-const int BAUD_RATE = 115200;
+#define LED_PIN_GREEN 7
+#define LED_PIN_RED 8
+#define BAUD_RATE 115200
+
 const int BLINK_SPEED = 1000;
 
 // the setup function runs once when you press reset or power the board
