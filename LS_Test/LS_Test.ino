ec5c4e8419a007943d1d029b11154d575ff2a29d
Adam Clothier <adam.clothier@eagles.oc.edu>
2015-09-30 15:14:43 -0500

    Wrote LS Test
    Full LS test using Serial Monitor

--- LS_Test/LS_Test.ino
+++ LS_Test/LS_Test.ino
@@ -1,23 +1,34 @@
 /*
  * Light Sensor Test
- * Author: Corbin Murrow
- * Date: 25 September 2015
- * Version: 1.0
+ * Author: Corbin Murrow, Adam Clothier
+ * Date: 30 September 2015
+ * Version: 1.1
  * 
- * This softwrae will test the photoresistors by reading the
+ * This software will test the photoresistors by reading the
  * value from an analog input pin and printing the value to the
  * serial monitor in the format "<Left/Right> Light: <value>".
  * 
  * ======= VERSION HISTORY =======
+ * Version 1.1: Wrote code for LS test, verified using only Serial Monitor - AC - 29 September 2015
  * Version 1.0: Created file and added header - CM - 25 September 2015
  */
 
-void setup() {
-  // put your setup code here, to run once:
-
+int LDRL = 5;
+int LDRR = 4;
+void setup()
+{
+    Serial.begin(115200);  //Begin serial communcation
+    pinMode( 7, OUTPUT );
+    pinMode( LDRL, INPUT);
+    pinMode( LDRR, INPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+ void loop() {
+   int v = analogRead(LDRL);
+   int w = analogRead(LDRR);
+   Serial.print("Left Photo-Resistor: ");
+   Serial.println(v);
+   Serial.print("Right Photo-Resistor: ");
+   Serial.println(w);
+   delay(1000);
+ }
